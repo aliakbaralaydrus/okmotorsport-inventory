d0bf3484792d4d7a3b4a9de17146da4905fd975c
ali alaydrus <aliakbaralaydrus123@gmail.com>
2025-10-25 16:53:59 -0700

    Fix Google Sheets public access

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import FSAEInventory from './FSAEInventory'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FSAEInventory from "./FSAEInventory";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <FSAEInventory scriptUrl="" />
-  </React.StrictMode>,
-)
+    <FSAEInventory scriptUrl="https://script.google.com/macros/s/AKfycbxIPvQXbaPKNa-PyESRiTjkOm0PWT__0ff-OTmPNyxmbjQqpiV2X-Um-YHGFskNjwaA/exec" />
+  </React.StrictMode>
+);
