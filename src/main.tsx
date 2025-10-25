6156c190dc5739cd1738b23095bf765419b5b910
ali alaydrus <aliakbaralaydrus123@gmail.com>
2025-10-25 16:15:55 -0700

    Initial commit - OK Motorsport Inventory

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import FSAEInventory from './FSAEInventory'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <FSAEInventory scriptUrl="" />
+  </React.StrictMode>,
+)
